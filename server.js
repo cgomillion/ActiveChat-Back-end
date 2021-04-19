62cca1e0a7929c5a5451ebbb9b867414d25024be
Gioele Rossi <gioelerossi@Gioeles-Mac-mini.local>
2021-04-19 19:32:15 +0100

    changed server.js referencing holiday app

--- server.js
+++ server.js
@@ -1,37 +1,67 @@
+//DEPENDENCIES
 const express = require('express');
 const app = express();
-const PORT = 3005;
+const PORT = 3003;
 const mongoose = require('mongoose');
-const cors = require('cors');
-const chatController= require('./controllers/chatController')
+const cors = require('cors')
+const session = require('express-session');
 
 // MIDDLEWARE
+// this will tell the server to parse the JSON data, and create the req.body object.
 app.use(express.json());
 
-//models
-const Chats = require('./models/chatModel')
-const Topics= require('./models/topicModel')
+// Setup Cors middleware
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+	origin: (origin, callback) => {
+		if (whitelist.indexOf(origin) !== -1 || !origin) {
+			callback(null, true)
+		} else {
+			callback(new Error('Not allowed by CORS'))
+		}
+	},
+	credentials:true
+}
 
-// SETUP Mongoose
-mongoose.connect('mongodb://localhost:27017/activechatDB',{
+app.use(cors(corsOptions))
+
+
+
+// SETUP mongoose
+mongoose.connect('mongodb://localhost:27017/chatroomDB',{
 	useNewUrlParser:true,
 	useUnifiedTopology: true,
-  useFindAndModify: false
+	useFindAndModify: false
 });
 
-// set up listeners to monitor your database connection
+// set up listeners to monitor your DB connection
 const db = mongoose.connection;
 db.once('open', ()=> console.log('DB connected...'));
-db.on('error', (err)=> console.log(err.message));
-db.on('disconnected', ()=> console.log('mongoose disconnected'));
-
-// CONTROLLERS
-app.use('/chat', require('./controllers/chatController'));
-
-app.use('/topic', require('./controllers/topicController'));
+db.on('error', (error)=> console.log(error.message));
+db.on('disconnected', ()=> console.log('Mongoose disconnected...'));
 
 
+/
+app.use(session({
+	secret: 'JustKiding',
+	resave: false,
+	saveUninitialized: false
+}))
 
-app.listen(PORT, () => {
-  console.log('ActiveChat listening at:', PORT, '🎉🎊',)
+const isAuthenticated = (req, res, next) => {
+    if (req.session.currentUser) {
+        return next()
+    } else {
+        res.status(403).json({msg:"loging require"})
+    }
+}
+
+
+// controllers
+app.use('/chat', isAuthenticated,  require('./controllers/chatController'))
+app.use('/topics', require('./controllers/topicsController'))
+
+
+app.listen(PORT, ()=>{
+	console.log(`Server is listening on port ${PORT}`);
 })
